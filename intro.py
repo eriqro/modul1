834af1b84b1ce1ba023ad494c9f1516e4a83ed42
eriqro <142878819+eriqro@users.noreply.github.com>
2023-09-07 13:15:16 +0200

    ee

---
+++ intro.py
@@ -0,0 +1,3 @@
+x=input("Namn: ")
+y=input("Ålder: ")
+print("Hej ",x,"du är",y,"gammal")
---
+++ modu7k1.py
@@ -0,0 +1,20 @@
+print("hello world")
+print("kungen av \"nacka\"")
+print("""Lorem Ipsu
+Lorem Ipsu
+Lorem Ipsu
+Lorem Ipsu§
+Lorem Ipsu
+Lorem Ipsu
+Lorem Ipsu
+Lorem Ipsu
+Lorem Ipsu
+Lorem Ipsu""")
+print("jag\nHatar\npotatisar")
+print("Du är"+" en potatis")
+print("Du är ",10+2," åring")
+a=input("Hur gammal är du")
+print("Du är ",a," år gammal")
+b=input("Vad heter du? ")
+c=input("Vilket land kommer du ifrån? ")
+print("Hej ",b," du är ",a," år gammal och kommer ifrån ",c)
